380d50686c6cf829b6e70949eee81189f6c95d87
PervertKun <shivenderkumar761@gmail.com>
2021-03-16 15:29:33 +0530

    title

--- src/Components/Covid_Data/Covid_Data.js
+++ src/Components/Covid_Data/Covid_Data.js
@@ -5,6 +5,7 @@ const CovidData = (data) => {
   return (
     data.data !== undefined && (
       <React.Fragment>
+        <h2 id="map_title">State wise Cases in India</h2>
         <div className="home">
           <table id="simple-board">
             <tbody>{stateTable(data.data)}</tbody>
